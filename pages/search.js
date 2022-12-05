aee9c7e7a808bf411e9aa33dadea8fa0d7db605e
alainaczachariah <105684730+alainaczachariah@users.noreply.github.com>
2022-12-05 13:36:57 -0500

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,130 +1,132 @@
-import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import MainNav from "../components/MainNav";
-import { useEffect, useState } from "react";
-
+import { useRouter } from "next/router";
+import { searchHistoryAtom } from "../store";
 import { useAtom } from "jotai";
-import { searchHistroyAtom } from "../store";
-import { addToHistory } from "../lib/userData";
+import { useState } from 'react';
+import { Row, Col, Form, Button } from "react-bootstrap";
+import { addToHistory } from "../lib/UserData";
 
-
-export default function AdvancedSearch(props) {
+export default function AdvancedSearch() {
   const router = useRouter();
-  const { searchHistory, setSearchHistory } = useAtom(searchHistroyAtom);
+  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+
   const {
     register,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm({
     defaultValues: {
-      searchBy: "title",
+      q: "",
       geoLocation: "",
       medium: "",
-      isOnView: "true",
-      isHighlight: "false",
-      q: "",
+      searchBy: [],
+      isOnView: false,
+      isHighlight: false,
     },
   });
-  useEffect(() => {
-    let data = {
-      searchBy: "title",
-      geoLocation: "",
-      medium: "",
-      isOnView: "true",
-      isHighlight: "false",
-      q: "",
-    };
-    for (const prop in data) {
-      setValue(prop, data[prop]);
-    }
-  }, []);
-  async function submitForm(data) {
-    let queryString = "";
-    queryString += `searchBy=${data.searchBy}`;
-    if (data.geoLocation) {
-      queryString += `&geoLocation=${data.geoLocation}`;
-    }
-    if (data.medium) {
-      queryString += `&medium=${data.medium}`;
-    }
-    queryString += `&isOnView=${data.isOnView}`;
-    queryString += `&isHighlight=${data.isHighlight}`;
-    queryString += `&q=${data.q}`;
 
-    setSearchHistory(await addToHistory(queryString));
-    router.push(`/artwork?${queryString}`);
-  }
+  async function submitForm(data) {
+    let finalQuery = `${data.searchBy + "=true"}&isOnView=${
+      data.isOnView
+    }&isHighlight=${data.isHighlight}${
+      data.geoLocation ? "&geoLocation=" + data.geoLocation : ""
+    }${data.medium ? "&medium=" + data.medium : ""}&q=${data.q}`;
+
+    router.push("artwork?" + finalQuery);
+    setSearchHistory(await addToHistory("artwork?" + finalQuery))
+}
 
   return (
     <>
-      <MainNav />
-      <h1>Advanced Search</h1>
       <Form onSubmit={handleSubmit(submitForm)}>
-        <Col>
-          <Form.Group as={Row} className="mb-3" controlId="searchBy">
-            <Form.Label column sm={2}>
-              Search By
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Select {...register("searchBy")}>
-                <option value="title">Title</option>
-                <option value="tags">Tags</option>
-                <option value="artistOrCulture">Artist/Culture</option>
-              </Form.Select>
-            </Col>
-            <Col sm={10}>
-              <Form.Group className="mb-3" controlId="geoLocation">
-                <Form.Label>Geo Location</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Geo Location"
-                  {...register("geoLocation")}
-                />
-                <Form.Text className="text-muted">
-                  Case Sensitive String (ie &quot;Europe&quot;,
-                  &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;,
-                  &quot;New York&quot;, etc.), with multiple values separated by
-                  the | operator
-                </Form.Text>
-              </Form.Group>
-            </Col>
-            <Col sm={10}>
-              <Form.Group className="mb-3" controlId="medium">
-                <Form.Label>Medium</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter Medium"
-                  {...register("medium")}
-                />
-                <Form.Text className="text-muted">
-                  Case Sensitive String (ie &quot;Oil on Canvas&quot;,
-                  &quot;Bronze&quot;, &quot;Wood&quot;, &quot;Stone&quot;,
-                  &quot;Paper&quot;, etc.), with multiple values separated by
-                  the | operator
-                </Form.Text>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Check
-                type="checkbox"
-                label="On View"
-                {...register("isOnView")}
+        <Row>
+          <Col>
+            <Form.Group className="mb-3">
+              <Form.Label>Search Query</Form.Label>
+              <Form.Control
+                className={errors.q ? "is-invalid" : ""}
+                type="text"
+                {...register("q", { required: true })}
+                placeholder=""
+                name="q"
               />
-              <Form.Check
-                type="checkbox"
-                label="Highlight"
-                {...register("isHighlight")}
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <Form.Label>Search By</Form.Label>
+            <Form.Select
+              name="searchBy"
+              className="mb-3"
+              {...register("searchBy")}
+            >
+              <option value="title">Title</option>
+              <option value="tags">Tags</option>
+              <option value="artistOrCulture">Artist or Culture</option>
+            </Form.Select>
+          </Col>
+          <Col md={4}>
+            <Form.Group className="mb-3">
+              <Form.Label>Geo Location</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                name="geoLocation"
+                {...register("geoLocation")}
               />
-            </Col>
+              <Form.Text className="text-muted">
+                Case Sensitive String (ie &quot;Europe&quot;,
+                &quot;France&quot;, &quot;Paris&quot;, &quot;China&quot;,
+                &quot;New York&quot;, etc.), with multiple values separated by
+                the | operator
+              </Form.Text>
+            </Form.Group>
+          </Col>
+          <Col md={4}>
+            <Form.Group className="mb-3">
+              <Form.Label>Medium</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder=""
+                name="medium"
+                {...register("medium")}
+              />
+              <Form.Text className="text-muted">
+                Case Sensitive String (ie: &quot;Ceramics&quot;,
+                &quot;Furniture&quot;, &quot;Paintings&quot;,
+                &quot;Sculpture&quot;, &quot;Textiles&quot;, etc.), with
+                multiple values separated by the | operator
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form.Check
+              type="checkbox"
+              label="Highlighted"
+              name="isHighlight"
+              {...register("isHighlight")}
+            />
+            <Form.Check
+              type="checkbox"
+              label="Currently on View"
+              name="isOnView"
+              {...register("isOnView")}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
             <br />
             <Button variant="primary" type="submit">
               Submit
             </Button>
-          </Form.Group>
-        </Col>
+          </Col>
+        </Row>
       </Form>
     </>
   );
 }
+
